dbfa665ec8c53d3bc01b0f7151895aa169cf2dd7
Jesse Wilker <X110019@avidxchange.com>
2020-04-08 12:08:20 -0400

    Adding SQL Server for each functionality

---
+++ ThreeTierApplication/SqlServer/2-SQLServer.tf
@@ -0,0 +1,158 @@
+provider "azurerm" {
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  version         = "~>1.32.0"
+}
+
+locals {
+  #Local variables 
+  sqlServer = {
+    server = {
+      serverName = "${var.resource_prefix}-sql"
+      nicId      = module.deploy_nic_sql.id
+      sku        = "Standard_D4_v3"
+    }
+    server1 = {
+      serverName = "${var.resource_prefix}-sql1"
+      nicId      = module.deploy_nic_sql1.id
+      sku        = "Standard_D4_v3"
+    }
+    server2 = {
+      serverName = "${var.resource_prefix}-sql2"
+      nicId      = module.deploy_nic_sql2.id
+      sku        = "Standard_D2_v3"
+    }
+  }
+}
+
+data "azurerm_virtual_network" "coreVnet" {
+  name                = "core-vnet"
+  resource_group_name = "core-networking"
+}
+
+data "azurerm_subnet" "subnetIds" {
+  for_each             = toset(data.azurerm_virtual_network.coreVnet.subnets)
+  name                 = each.key
+  virtual_network_name = data.azurerm_virtual_network.coreVnet.name
+  resource_group_name  = data.azurerm_virtual_network.coreVnet.resource_group_name
+}
+
+#ResourceGroup
+module "deployRGCompute" {
+  source              = "../modules/New-ResourceGroup"
+  resource_group_name = "${var.resource_prefix}-rg"
+  location            = var.location
+  resource_tags       = var.resource_tags
+}
+
+#Build Web Server Nic Card 
+
+module "deploy_nic_sql" {
+  source                        = "../modules/New-NetworkInterface"
+  resource_group_name           = module.deployRGCompute.name
+  location                      = module.deployRGCompute.location
+  nic_name                      = "${var.resource_prefix}-nic-sql"
+  ip_name                       = "${var.resource_prefix}-ip1-sql"
+  subnet_id                     = data.azurerm_subnet.subnetIds["subnetSql"].id
+  private_ip_address_allocation = var.private_ip_address_allocation
+  resource_tags                 = var.resource_tags
+}
+
+#Build Web Server Nic Card 
+
+module "deploy_nic_sql1" {
+  source                        = "../modules/New-NetworkInterface"
+  resource_group_name           = module.deployRGCompute.name
+  location                      = module.deployRGCompute.location
+  nic_name                      = "${var.resource_prefix}-nic-sql1"
+  ip_name                       = "${var.resource_prefix}-ip1-sql1"
+  subnet_id                     = data.azurerm_subnet.subnetIds["subnetSql"].id
+  private_ip_address_allocation = var.private_ip_address_allocation
+  resource_tags                 = var.resource_tags
+}
+
+module "deploy_nic_sql2" {
+  source                        = "../modules/New-NetworkInterface"
+  resource_group_name           = module.deployRGCompute.name
+  location                      = module.deployRGCompute.location
+  nic_name                      = "${var.resource_prefix}-nic-sql2"
+  ip_name                       = "${var.resource_prefix}-ip1-sql2"
+  subnet_id                     = data.azurerm_subnet.subnetIds["subnetSql"].id
+  private_ip_address_allocation = var.private_ip_address_allocation
+  resource_tags                 = var.resource_tags
+}
+
+
+#SQL Virtual Machines
+
+resource "azurerm_virtual_machine" "servers" {
+  for_each              = local.sqlServer
+  name                  = each.value.serverName
+  location              = module.deployRGCompute.location
+  resource_group_name   = module.deployRGCompute.name
+  network_interface_ids = [each.value.nicId]
+  vm_size               = each.value.sku
+
+  # This means the OS Disk will be deleted when Terraform destroys the Virtual Machine
+  # NOTE: This may not be optimal in alls cases.
+  delete_os_disk_on_termination = true
+
+  # This means the Data Disk Disk will be deleted when Terraform destroys the Virtual Machine
+  # NOTE: This may not be optimal in all cases.
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "${each.value.serverName}-osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  # Optional data disks
+  storage_data_disk {
+    name              = "${each.value.serverName}-datadisk"
+    create_option     = "Empty"
+    disk_size_gb      = "1023"
+    lun               = 0
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = each.value.serverName
+    admin_username = "jwilk-admin"
+    admin_password = "admin123-AAA"
+  }
+  os_profile_windows_config {
+    provision_vm_agent        = true
+    enable_automatic_upgrades = true
+    timezone                  = "Eastern Standard Time"
+  }
+
+
+}
+
+
+output "coreVnet" {
+  value = data.azurerm_virtual_network.coreVnet.id
+}
+
+output "subnetIds" {
+  value = [for value in data.azurerm_subnet.subnetIds : value.id]
+}
+
+output "nic_sql" {
+  value = module.deploy_nic_sql.id
+}
+
+output "servers" {
+  value = [for servers in azurerm_virtual_machine.servers : servers.id]
+}
---
+++ ThreeTierApplication/SqlServer/variables.tf
@@ -0,0 +1,47 @@
+/* Azure Environment Variables*/
+variable "subscription_id"{
+	description = "Enter Subscription Id for the Azure Subscription"
+}
+variable "client_id"{
+	description = "Enter Azure AD Application id"
+}
+variable "client_secret"{
+	description = "Enter Azure AD Application secret"
+}
+variable "tenant_id"{
+	description = "Enter Azure AD Application Tenant Id"
+}
+variable "location"{
+	description = "Enter location for resource group"
+	default = "EAST US"
+}
+
+
+#Resource Group Variables
+variable "resource_prefix"{
+	description = "Enter resource prefix name"
+	default = "jwilkInc"
+}
+
+
+variable "resource_tags" {
+	  type = map(string)
+  description = "A map of the tags to use on the resources that are deployed with this module."
+  default = {
+			"BudgetCategory"  = "DevResources"
+			"Owner"           = "Jwilk"
+			"TeamName"        = "DEVOPS"
+			"Department"      = "ENGINEERING"
+			"Program"         = "Terraform Development"
+			"Environment"     = "DEV"
+  }
+}
+
+#Vnet
+
+variable "private_ip_address_allocation" {
+	description = "how a private ip address will be assigned to nic, valid values - Static,Dynamic"
+    default = "Dynamic"
+    }
+
+#Subnet
