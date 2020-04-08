d7310bb76b2af65388a6808a67443a6794f80ae4
Jesse Wilker <X110019@avidxchange.com>
2020-04-08 14:39:43 -0400

    checking in local

--- ThreeTierApplication/backend.tf
+++ ThreeTierApplication/backend.tf
@@ -1,10 +1,11 @@
-/*terraform {
+/*
+terraform {
  backend "azurerm" {
-	resource_group_name = "MyHomeLab"
-	storage_account_name = "ahterraformremotestate"
-	container_name = "terraformstate"
-	key = "LinuxVM.tfstate"
-	access_key = "[ENTER BLOB ACCESS KEY HERE]"
+	resource_group_name = "AvidX-DevQA-CloudAutomation-RG"
+	storage_account_name = "iacdvterraformsa01"
+	container_name = "terraform-state"
+	key = "avidx-avidsuite-dev-az.tfstate"
+	access_key = "WFPTM5aG4gKlXDl8LElyXuj2ijUFCEXQo1v/47WBXbAmZjV0hP0A3++6uTeb2vdJ709GgR35Zh+wTQLSXZDpAQ=="
  }
 }
 */
