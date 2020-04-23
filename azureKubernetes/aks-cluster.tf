7ce8c0afd2a91ba43b6f0befd19b2432b15183e6
Jesse Wilker <X110019@avidxchange.com>
2020-04-23 17:07:53 -0400

    Adding Kubernetes

---
+++ azureKubernetes/aks-cluster.tf
@@ -0,0 +1,56 @@
+resource "random_pet" "prefix" {}
+
+variable "appId" {
+}
+
+variable "password" {
+}
+
+provider "azurerm" {
+  version = "~> 1.27.0"
+}
+
+resource "azurerm_resource_group" "default" {
+  name     = "${random_pet.prefix.id}-rg"
+  location = "West US 2"
+
+  tags = {
+    environment = "Demo"
+  }
+}
+
+resource "azurerm_kubernetes_cluster" "default" {
+  name                = "${random_pet.prefix.id}-aks"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+  dns_prefix          = "${random_pet.prefix.id}-k8s"
+
+  agent_pool_profile {
+    name            = "default"
+    count           = 2
+    vm_size         = "Standard_D2_v2"
+    os_type         = "Linux"
+    os_disk_size_gb = 30
+  }
+
+  service_principal {
+    client_id     = var.appId
+    client_secret = var.password
+  }
+
+  role_based_access_control {
+    enabled = true
+  }
+
+  tags = {
+    environment = "Demo"
+  }
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.default.name
+}
+
+output "kubernetes_cluster_name" {
+  value = azurerm_kubernetes_cluster.default.name
+}
